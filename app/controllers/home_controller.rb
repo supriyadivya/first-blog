1d46f66fd6938916313b6a255f68f795e634bef0
divya <patnamdivya.ad@gmail.com>
2015-02-24 15:02:36 +0530

    my new blog

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+  	def contact_us
+  
+end
+def licet
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :blogs
+
+  get "home" => "home#home"
+  get "about" => "home#about"
+  get"contact us" =>"home#contact us"
+  get "licet"=>"home#licet"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
